242950e53921460c8377b4b442271229ddc4dd71
tac-nayn06 <82848917+tac-nayn06@users.noreply.github.com>
2021-07-10 14:11:26 -0700

    pell equation program

--- python/nt-scripts.py
+++ python/nt-scripts.py
@@ -1,4 +1,4 @@
-import math
+import math as m
 def legendre(a,p):
     ans=pow(a,(p-1)//2,p)
     if ans==1: return True
@@ -29,12 +29,12 @@ def twosquaresum(p): #solving diophantine eq x^2+y^2=p for primes, criterion is 
     l=ressol(p-1,p)
     S=[[0,-1],[1,0]]
     s=min(l[0],l[1])
-    k=math.ceil(s*(s/p))
+    k=m.ceil(s*(s/p))
     a,b,c=p,2*s,k
     v=[[1],[0]]
     while (a,b,c) !=(1,0,1):
         if a>c: a,b,c,v =c,-b,a,multiply(S,v)
-        m=math.ceil(-b/(2*a))
+        m=m.ceil(-b/(2*a))
         b,c=2*a*m+b,(a*m*m)+(b*m)+c
         v=multiply([[1,-m],[0,1]],v)
     return [abs(v[0][0]),abs(v[1][0])]
@@ -48,11 +48,33 @@ def x2_xy_y2(p):# solves diophantine eq x^2+xy+y^2=p, prime p
     v,S=[[1],[0]],[[0,-1],[1,0]]
     while (a,b,c) !=(1,1,1):
         if a>c: a,b,c,v =c,-b,a,multiply(S,v)
-        if b<0: m=math.floor(-b/(2*a)+0.5)
-        else: m=math.ceil(-b/(2*a)-0.5)
+        if b<0: m=m.floor(-b/(2*a)+0.5)
+        else: m=m.ceil(-b/(2*a)-0.5)
         b,c=2*a*m+b,(a*m*m)+(b*m)+c
         v=multiply([[1,-m],[0,1]],v)
     p,q=v[0][0],v[1][0]
     if p*q > 0: return [abs(p),abs(q)]
     elif p+q<0: return [max(p,q),-(p+q)]
     return [-min(p,q),p+q]
+def contfrac(d): #contfrac expansion of sqrt(d), d nonsquare
+    f=m.floor(m.sqrt(d))
+    ans=[[f],[]]
+    a,b=f,d-f*f
+    while b !=1:
+        ans[1].append((a+f)//b)
+        a=((a+f)//b)*b-a
+        b=(d-a*a)//b
+    ans[1].append(2*f)
+    return ans
+def evalcf(A): #A in contfrac form outputted from contfrac
+    h1,h2,k1,k2=1,A[0][0],0,1
+    for i in range(len(A[1])):
+        h1,h2=h2,A[1][i]*h2+h1
+        k1,k2=k2,A[1][i]*k2+k1
+    return [h2,k2]
+def pellsolve(d): #solves pell equation x^2-dy^2=1 in integers, outputs fundamental sol
+    L=contfrac(d)
+    L[1].pop()
+    h,k=evalcf(L)[0],evalcf(L)[1]
+    if h*h-d*k*k==1: return [h,k]
+    return [h*h+d*k*k,2*h*k]
