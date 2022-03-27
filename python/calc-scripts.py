4edc28176b5eacb94bdfdc4653a894bc7eb7ff4f
tac-nayn06 <82848917+tac-nayn06@users.noreply.github.com>
2022-03-27 13:05:46 -0700

    fix more errors

--- python/calc-scripts.py
+++ python/calc-scripts.py
@@ -3,36 +3,36 @@ import math as m
 def f1(x): return x*x-4 #change this to your discretion, to be referenced in 'fakeintegral' functions;
 def derivative(r,n):
     '''1/n = horizontal length of secant segment'''
-    return n*(f(r+1/n)-f(r))
+    return n*(f1(r+1/n)-f1(r))
 def lowfakeintegral(a,b,n):
     '''a,b are endpts, n is number of sections'''
     ans,intervalsize=0,(b-a)/n
     for i in range(n):
-        ans+=f(a+i*intervalsize)
+        ans+=f1(a+i*intervalsize)
     return ans*intervalsize
 def highfakeintegral(a,b,n):
     '''a,b are endpts, n is number of sections'''
     ans,intervalsize=0,(b-a)/n
     for i in range(n):
-        ans+=f(a+(i+1)*intervalsize)
+        ans+=f1(a+(i+1)*intervalsize)
     return ans*intervalsize
 def trapezoid(a,b,n):
     ans,intervalsize=0,(b-a)/n
     for i in range(n):
-        ans+=f(a+i*intervalsize)a+f(a+(i+1)*intervalsize)
+        ans+=f1(a+i*intervalsize)+f1(a+(i+1)*intervalsize)
     return ans*intervalsize/2
 def simpson(a,b,n):
     '''n MUST be even for simpson to work!'''
-    ans,intervalsize=f(a)-f(b),(b-a)/n
+    ans,intervalsize=f1(a)-f1(b),(b-a)/n
     for i in range(n//2):
-        ans+=4*f(a+(2*i+1)*intervalsize) + 2*f(a+(2*i+2)*intervalsize)
+        ans+=4*f1(a+(2*i+1)*intervalsize) + 2*f1(a+(2*i+2)*intervalsize)
     return ans*intervalsize/3
 def newton(g,m):
     '''m iterations'''
     N=10**15
     guess=g
     for i in range(m):
-        line= [derivative(guess,N),guess*derivative(guess,N)-f(guess)] # tangent line: y=l[0]*x-l[1]
+        line= [derivative(guess,N),guess*derivative(guess,N)-f1(guess)] # tangent line: y=l[0]*x-l[1]
         guess=line[1]/line[0]
     return guess
 def f2(x,y): return m.e**(x*y)
